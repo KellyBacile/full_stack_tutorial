16b3912ab7d5569b8dea2686f56677d4c7f612ab
Kelly B. <kj.bacile@gmail.com>
2025-06-09 21:43:09 -0500

    project load

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('dist'))
+app.use(express.json())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  const generateId = () => {
+    const maxId = notes.length > 0 
+      ? Math.max(...notes.map(n => Number(n.id))) 
+      : 0
+    return String(maxId + 1)
+  }
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.put('/api/notes/:id', (req, res) => {
+    const id = req.params.id
+    const newNote = req.body
+    notes = notes.map(note => note.id === id ? newNote : note)
+
+    res.json(newNote)
+})
+
+
+const unknownEndpoint = (req, res) => {
+  console.log('error: unknown endpoint')
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
